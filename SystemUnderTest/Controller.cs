216bf3407b7aa5501741895b89770320dad6f013
Sergey Rogovtsev <srogovtsev@acumatica.com>
2023-01-07 08:44:50 -0500

    Added Controller to be tested (a very naive version of some legacy code)

---
+++ SystemUnderTest/Controller.cs
@@ -0,0 +1,50 @@
+﻿namespace SystemUnderTest;
+
+public interface IRepository
+{
+    (string expectedCode, bool isMobile, Uri redirect) GetState(string login);
+}
+
+public class Controller
+{
+    private readonly IRepository _repository;
+
+    public Controller(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public string Complete(string state, string code)
+    {
+        var knownState = _repository.GetState(state);
+        try
+        {
+            if (Validate(code, knownState))
+            {
+                if (knownState.isMobile)
+                    return "{\"success\": true, \"redirect\": \"" + knownState.redirect + "\"}";
+                else
+                    return "302 Location: " + knownState.redirect;
+            }
+            else
+            {
+                if (knownState.isMobile)
+                    return "{\"success\": false, \"redirect\": \"login\"}";
+                else
+                    return "302 Location: login";
+            }
+        }
+        catch (Exception e)
+        {
+            if (knownState.isMobile)
+                return "{\"error\": \"" + e.Message + "\"}";
+            else
+                return "500";
+        }
+    }
+
+    private static bool Validate(string code, (string expectedCode, bool isMobile, Uri redirect) knownState)
+    {
+        return code == knownState.expectedCode;
+    }
+}
